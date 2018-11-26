980fad5df31a5dda5aaf3cc0b78141142b646c57
Vlad <bruno993409@gmail.com>
2018-11-26 20:19:16 +0200

    renaming main to spash_screen

--- app/src/main/java/cf/flymengo/milionaire_attempt2/spash_screen.java
+++ app/src/main/java/cf/flymengo/milionaire_attempt2/spash_screen.java
@@ -3,11 +3,11 @@ package cf.flymengo.milionaire_attempt2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class spash_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
     }
 }
